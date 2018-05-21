255adae51ded80c05c4ea1be234b8ab41c58d1e3
petrozvodsky <petrozavodsky@gmail.com>
2018-05-21 16:58:50 +0300

    fix code style

--- admin/options.php
+++ admin/options.php
@@ -1,53 +1,49 @@
 <?php
 
-class Bro_Excerpt_Length_Options
-{
+class Bro_Excerpt_Length_Options {
 
 	private $field = 'default_bro_excerpt_length';
 
-	public function __construct()
-	{
-		add_action('admin_init', array($this, 'add_option_field_to_general_admin_page'));
+	public function __construct() {
+		add_action( 'admin_init', array( $this, 'add_option_field_to_general_admin_page' ) );
 	}
 
 
-	function add_option_field_to_general_admin_page()
-	{
+	function add_option_field_to_general_admin_page() {
 
-		register_setting('writing', $this->field, 'intval');
+		register_setting( 'writing', $this->field, 'intval' );
 
 		add_settings_field(
 			$this->field,
-			__('The number of characters in the excerpt', 'bro_excerpt_length'),
-			array($this, 'callback'),
+			__( 'The number of characters in the excerpt', 'bro_excerpt_length' ),
+			array( $this, 'callback' ),
 			'writing',
 			'default',
 			array(
-				'id' => $this->field . '-id',
+				'id'          => $this->field . '-id',
 				'option_name' => $this->field
 			)
 		);
 	}
 
-	function callback($val)
-	{
-		$id = $val['id'];
+	function callback( $val ) {
+		$id          = $val['id'];
 		$option_name = $val['option_name'];
+
 		?>
         <input
                 type="number"
-                name="<? echo $option_name ?>"
-                id="<? echo $id ?>"
-                value="<? echo esc_attr(get_option($option_name, 50)) ?>"
+                name="<?php echo $option_name; ?>"
+                id="<?php echo $id; ?>"
+                value="<?php echo esc_attr( get_option( $option_name, 50 ) ); ?>"
         />
 		<?php
 	}
 
 }
 
-function bro_excerpt_length_options_init()
-{
+function bro_excerpt_length_options_init() {
 	new Bro_Excerpt_Length_Options();
 }
 
-add_action('plugins_loaded', 'bro_excerpt_length_options_init');
+add_action( 'plugins_loaded', 'bro_excerpt_length_options_init' );
